06022ec4dec262cd808a9346b65cea2246e10fc5
matteorlt <matteo.rannouletexier2025@campus-eni.fr>
2025-04-11 23:54:55 +0200

    Start of the memory

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-$(document).ready(function () {
-    // Animation du bouton "En savoir plus"
-    $("#learn-more").click(function () {
-        $("html, body").animate({
-            scrollTop: $("#about").offset().top
-        }, 1000);
-    });
-
-    // Animation de la section "À propos"
-    $("#about").hide().fadeIn(2000);
-
-    // Changement de couleur de fond lors du survol des liens de navigation
-    $("nav ul li a").hover(function () {
-        $(this).css("background-color", "#ff6f61");
-    }, function () {
-        $(this).css("background-color", "transparent");
-    });
-});
+function cardsMemory()  {
+    element = $('#monID');
+    }
