79ea27dea201d58a26d0c8ddc3f6778c523f4b44
matteorlt <matteo.rannouletexier2025@campus-eni.fr>
2025-04-12 00:36:04 +0200

    function create

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
-function cardsMemory()  {
-    element = $('#monID');
-    }
+function startGame() {
+    element = $('.btn btn-primary custom-btn');
+    element.addEventListener('click', cardsMemory);
+    maxTwoCards();
+    shuffleCards();
+}
+function cardsClicked() {
+    
+}
+
+function cardsFlip()  {
+    
+}
+
+function maxTwoCards() {
+
+}
+
+function twoCardsAreTheSame() {
+
+}
+
+function shuffleCards(){
+
+}
+
+function cardsPlace() {
+
+}
