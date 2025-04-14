f7e107bc509aaaeb533b4c87558e29110bd5eb6f
Mattéo Rannou <matteo.rannouletexier2025@campus-eni.fr>
2025-04-14 10:48:16 +0200

    Function setupCardClicks et startGame fini

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
+$(document).ready(function () {
+  // Quand on clique sur le bouton, on démarre la partie
+  $(".btn.btn-primary.custom-btn").on("click", function () {
+    startGame();
+  });
+});
+
+// Fonction pour démarrer le jeu
 function startGame() {
-    element = $('.btn btn-primary custom-btn');
-    element.addEventListener('click', cardsMemory);
-    maxTwoCards();
-    shuffleCards();
-}
-function cardsClicked() {
-    
+  console.log("start");
+  setupCardClicks();
+
+  // maxTwoCards();
+  // shuffleCards();
 }
 
-function cardsFlip()  {
-    
-}
+// Fonction pour gérer les clics sur les cartes
+function setupCardClicks() {
+    $(".img-cell").on("click", function () {
+      if (!$(this).hasClass("clicked")) {
+        let randomNum = Math.floor(Math.random() * 10);
+        let imgListe = [
+          "Ball.png",
+          "Bands.png",
+          "Bars.png",
+          "Champi.png",
+          "Flower.png",
+          "Hearth.png",
+          "Leaves.png",
+          "Star.png",
+          "Waves.png",
+          "Yellow_flower.png",
+        ];
+        $(this)
+          .find("img")
+          .attr("src", "images/" + imgListe[randomNum]);
+        $(this).addClass("clicked");
+      }
+    });
+  }
 
-function maxTwoCards() {
+function maxTwoCards() {}
 
-}
+function twoCardsAreTheSame() {}
 
-function twoCardsAreTheSame() {
+function shuffleCards() {}
 
-}
-
-function shuffleCards(){
-
-}
-
-function cardsPlace() {
-
-}
+function cardsPlace() {}
