35a16863cd25ed0e63b15866381c1cd4e4d4f556
Mattéo Rannou <matteo.rannouletexier2025@campus-eni.fr>
2025-04-14 12:00:48 +0200

    ajout des pages + css

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 function startGame() {
   console.log("start");
   setupCardClicks();
+  maxTwoCards()
 
   // maxTwoCards();
   // shuffleCards();
@@ -16,30 +17,49 @@ function startGame() {
 
 // Fonction pour gérer les clics sur les cartes
 function setupCardClicks() {
-    $(".img-cell").on("click", function () {
-      if (!$(this).hasClass("clicked")) {
-        let randomNum = Math.floor(Math.random() * 10);
-        let imgListe = [
-          "Ball.png",
-          "Bands.png",
-          "Bars.png",
-          "Champi.png",
-          "Flower.png",
-          "Hearth.png",
-          "Leaves.png",
-          "Star.png",
-          "Waves.png",
-          "Yellow_flower.png",
-        ];
-        $(this)
-          .find("img")
-          .attr("src", "images/" + imgListe[randomNum]);
-        $(this).addClass("clicked");
-      }
-    });
-  }
+  $(".img-cell").on("click", function () {
+    if (!$(this).hasClass("clicked")) {
+      let randomNumCardsClicks = maxTwoCards()[0];
+      let imgListe = [
+        "Ball.png",
+        "Bands.png",
+        "Bars.png",
+        "Champi.png",
+        "Flower.png",
+        "Hearth.png",
+        "Leaves.png",
+        "Star.png",
+        "Waves.png",
+        "Yellow_flower.png",
+      ];
+      $(this)
+        .find("img")
+        .attr("src", "images/" + imgListe[randomNumCardsClicks]);
+      $(this).addClass("clicked");
+    }
+  });
+}
 
-function maxTwoCards() {}
+  function maxTwoCards() {
+    let randomNum = 0;
+    let liste = [];
+    let count = {};
+  
+    for (let i = 0; i < 20; i++) {
+      randomNum = Math.floor(Math.random() * 10);
+      if (count[randomNum] === undefined) {
+        count[randomNum] = 1;
+      } else if (count[randomNum] < 2) {
+        count[randomNum]++;
+      } else {
+        i--;
+        continue;
+      }
+      liste.push(randomNum);
+    }
+  
+    return liste;
+  }
 
 function twoCardsAreTheSame() {}
 
